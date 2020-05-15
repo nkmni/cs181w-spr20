e9fb10b6fc6ab894b6c8ffb3c77e586ae98c8d9a
PieGuyX1000 <56211815+PieGuyX1000@users.noreply.github.com>
2020-05-15 04:12:24 -0600

    Added more method calls that are injected from chrome browser extension to local youtube videoplayer
    already plenty annoying as-is
    
    next steps:  integrate these videoplayer function calls with user supplied settings via options page
    
    Possibilities: random timer, player settings to screw with, etc.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 var player = document.getElementById("movie_player");
-player.click()// it works!!
+//player.click()
 player.mute()
-//player.setVolume()
-player.toggleFullscreen()
+player.pauseVideo()
+player.setPlaybackRate(1.5)
+player.seekTo(80)
+player.toggleFullscreen()//sometimes doesn't work to prevent non-user initiated fullscreen
+player.seekTo(40)
+player.unMute()
+player.setVolume(25)
+
+//player.click()
+//player.unMute()
+player.playVideo()
+player.setPlaybackRate(1.5)
+player.seekTo(80)
+player.seekTo(40)
+player.unMute()
+player.setVolume(25)
