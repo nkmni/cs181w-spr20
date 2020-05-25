72933b5f928173d73f1de7e9d39ee73a5b8e505a
PieGuyX1000 <56211815+PieGuyX1000@users.noreply.github.com>
2020-05-25 02:57:58 -0600

    Implemented basic UI
    Basic popup UI is implemented, user can input options and save them, the youtube player is manipulated heavily upon page load, still working on making the popup settings directly influence the youtube player in realtime, pause buttom should pause the player but doesn't

--- popup.js
+++ popup.js
@@ -1,21 +1,42 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-'use strict';
-
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//var player = document.getElementById("movie_player");
+// Saves options to chrome.storage
+function save_options() {
+  var level_of_annoyingness = document.getElementById('level_of_annoyingness').value;
+  var randomSpeed = document.getElementById('random_speed').checked;
+  var randomMuting = document.getElementById('random_muting').checked;
+  chrome.storage.sync.set({
+    level_of_annoyingness: level_of_annoyingness,
+    randomSpeed: randomSpeed,
+    randomMuting: randomMuting
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
   });
-};
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value level_of_annoyingness = 'medium' and randomSpeed = true.
+  chrome.storage.sync.get({
+    level_of_annoyingness: 'medium',
+    randomSpeed: true,
+    randomMuting: false
+  }, function(items) {
+    document.getElementById('level_of_annoyingness').value = items.level_of_annoyingness;
+    document.getElementById('random_speed').checked = items.randomSpeed;
+    document.getElementById('random_muting').checked = items.randomMuting;
+  });
+}
+
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
+
+document.getElementById('pause').addEventListener('click',
+    random_playback_speed(document));
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var player = document.getElementById("movie_player");
 //player.click()
-player.mute()
+//player.mute()
 player.pauseVideo()
 player.setPlaybackRate(1.5)
 player.seekTo(80)
@@ -17,3 +17,8 @@ player.seekTo(80)
 player.seekTo(40)
 player.unMute()
 player.setVolume(25)
+
+function random_playback_speed(document) {
+	//var player = document.getElementById("movie_player");
+	player.setPlaybackRate(.75)//not actually random, will be set to a random value after this is properly implemented
+}
