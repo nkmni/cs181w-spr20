668cca84d5dc5155f9017b4c9c2a331a6fa1601d
Neil Khemani <radixtwo@users.noreply.github.com>
2020-05-29 19:33:32 -0700

    Created stylistic popup. Created CSS file for popup. Minor changes to manifest.json and script.js

--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
 //var player = document.getElementById("movie_player");
 // Saves options to chrome.storage
 function save_options() {
-  var level_of_annoyingness = document.getElementById('level_of_annoyingness').value;
-  var randomSpeed = document.getElementById('random_speed').checked;
-  var randomMuting = document.getElementById('random_muting').checked;
+  var annoy_level = document.getElementById('annoy_level').value;
+  var random_speed = document.getElementById('random_speed').checked;
+  var random_mute = document.getElementById('random_mute').checked;
   chrome.storage.sync.set({
-    level_of_annoyingness: level_of_annoyingness,
-    randomSpeed: randomSpeed,
-    randomMuting: randomMuting
+    annoy_level: annoy_level,
+    random_speed: random_speed,
+    random_mute: random_mute
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -21,15 +21,15 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value level_of_annoyingness = 'medium' and randomSpeed = true.
+  // Use default value annoy_level = 'medium' and random_speed = true.
   chrome.storage.sync.get({
-    level_of_annoyingness: 'medium',
-    randomSpeed: true,
-    randomMuting: false
+    annoy_level: 'medium',
+    random_speed: true,
+    random_mute: false
   }, function(items) {
-    document.getElementById('level_of_annoyingness').value = items.level_of_annoyingness;
-    document.getElementById('random_speed').checked = items.randomSpeed;
-    document.getElementById('random_muting').checked = items.randomMuting;
+    document.getElementById('annoy_level').value = items.annoy_level;
+    document.getElementById('random_speed').checked = items.random_speed;
+    document.getElementById('random_mute').checked = items.random_mute;
   });
 }
 
