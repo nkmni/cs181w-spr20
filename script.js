831915451a22f462e464bad203df2d5edcd558a2
Neil Khemani <radixtwo@users.noreply.github.com>
2020-05-30 18:48:11 -0700

    Minor changes to contentscript.js, manifest.json, script.js, and the popup code

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 var player = document.getElementById("movie_player");
-//player.click()
-//player.mute()
-player.pauseVideo()
-player.setPlaybackRate(2)
-player.seekTo(80)
-player.toggleFullscreen()//sometimes doesn't work to prevent non-user initiated fullscreen
-player.seekTo(120)
-player.unMute()
-player.setVolume(25)
+//player.click();
+//player.mute();
+//player.pauseVideo();
+player.setPlaybackRate(2);
+//player.seekTo(80)
+//player.toggleFullscreen(); //sometimes doesn't work to prevent non-user initiated fullscreen
+//player.unMute();
+//player.setVolume(25);
+//player.playVideo();
 
-//player.click()
-//player.unMute()
-player.playVideo()
-player.setPlaybackRate(-1)
-player.seekTo(80)
-player.seekTo(40)
-player.unMute()
-player.setVolume(25)
-
+/*
 function random_playback_speed(document) {
 	//var player = document.getElementById("movie_player");
 	player.setPlaybackRate(.75)//not actually random, will be set to a random value after this is properly implemented
 }
+*/
