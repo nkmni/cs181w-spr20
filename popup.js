c68a26d363690dc45aa3de8d12e7f6b8f0502092
Neil Khemani <radixtwo@users.noreply.github.com>
2020-06-02 23:09:41 -0700

    Minor changes to popup.js

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
 
 
 function format_time(seconds_left) {
-    negate = seconds_left < 0;
+    var negate = seconds_left < 0;
     if (negate)
         seconds_left *= -1;
-    hours = (seconds_left / 3600) >> 0;
-    minutes = ((seconds_left - 3600*hours) / 60) >> 0;
-    seconds = seconds_left % 60;
+    var hours = (seconds_left / 3600) | 0;
+    var minutes = ((seconds_left - 3600*hours) / 60) | 0;
+    var seconds = seconds_left % 60;
     hours = hours ? hours + ":" : "";
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
-    time_string = hours + minutes + ":" + seconds;
+    var time_string = hours + minutes + ":" + seconds;
     if (negate) 
         time_string = '-' + time_string;
     return time_string;
@@ -21,29 +21,28 @@ function format_time(seconds_left) {
 function save_options() {
   var timer_duration = document.getElementById('timer_duration').value;
   var invalid_duration = (timer_duration < 0) || (timer_duration > 120);
-  timer_duration = timer_duration > 120 ? 120 : timer_duration;
-  timer_duration = timer_duration < 0 ? 0 : timer_duration;
+  timer_duration = timer_duration > 120 ? 120 : (timer_duration < 0 ? 0 : timer_duration);
   var annoy_level = document.getElementById('annoy_level').value;
   var random_speed = document.getElementById('random_speed').checked;
   var random_mute = document.getElementById('random_mute').checked;
-  chrome.storage.sync.get({'session_started': false}, function(items) {
-    if (!items.session_started)
+  chrome.storage.sync.get(['timer_duration', 'seconds_left'], function(items) {
+    if (items.timer_duration == items.seconds_left) {
         chrome.storage.sync.set({'seconds_left': 60*timer_duration});
+        document.getElementById("countdown").textContent = format_time(60*timer_duration);
+    }
   });
   chrome.storage.sync.set({
     'timer_duration': timer_duration,
     'annoy_level': annoy_level,
     'random_speed': random_speed,
     'random_mute': random_mute
-  }, function() {
-    document.getElementById("countdown").textContent = format_time(60*timer_duration);
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved';
-    if (invalid_duration)
-        status.textContent += ' (0 < timer < 120)';
-    setTimeout(function() { status.textContent = ''; }, 4000);
   });
+  // Update status to let user know options were saved.
+  var status = document.getElementById('status');
+  status.textContent = 'Options saved';
+  if (invalid_duration)
+      status.textContent += ' (0 < timer < 120)';
+  setTimeout(function() { status.textContent = ''; }, 2000);
 }
 
 // Restores select box and checkbox state using the preferences
@@ -80,7 +79,6 @@ function pause_clicked() {
 function reset_clicked() {
     chrome.storage.sync.get(['timer_duration', 'refresh_needed'], function(items) {
       chrome.storage.sync.set({
-        'session_started': false,
         'seconds_left': 60*items.timer_duration,
         'refresh_needed': items.refresh_needed+1
       });
